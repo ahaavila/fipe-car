fefbdd36e1857fa11bd59530617d7a0c0e3b4442
Augusto Ávila <augusto.avila@usemobile.xyz>
2021-09-05 16:47:36 -0300

    feat: add index style sheet

---
+++ styles/home.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 2.5rem 2.5rem;
+  text-align: center;
+`;
+
+export const SubTitle = styled.p`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+`;
+
+export const SearchDiv = styled.div`
+  background: var(--gray-500);
+  width: 35vw;
+  height: 35vh;
+  margin: 0 auto;
+`;
