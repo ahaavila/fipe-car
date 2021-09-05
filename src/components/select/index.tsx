eb22ece6c7c138958f5cb1a6dea29c5d1f138002
Augusto Ávila <augusto.avila@usemobile.xyz>
2021-09-05 18:31:32 -0300

    feat: add select static component

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -5,26 +5,26 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const select: React.FC = () => {
 
-  const UseStyles = makeStyles({
-    option: {
-      fontSize: 15,
-      '& > span': {
-        marginRight: 10,
-        fontSize: 18,
-      },
-    },
-  });
+  // const UseStyles = makeStyles({
+  //   option: {
+  //     fontSize: 15,
+  //     '& > span': {
+  //       marginRight: 10,
+  //       fontSize: 18,
+  //     },
+  //   },
+  // });
 
-  const classes = UseStyles();
+  // const classes = UseStyles();
 
   return (
     <Autocomplete 
       options={top4Films}
-      classes={{
-        option: classes.option,
-      }}
+      // classes={{
+      //   option: classes.option,
+      // }}
       getOptionLabel={(option) => option.title}
-      style={{ width: '25vw', margin: '0 auto' }}
+      style={{ width: '25vw', height: '1.5rem', margin: '0 auto', marginBottom: '2.5rem' }}
       renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
     />
   );
