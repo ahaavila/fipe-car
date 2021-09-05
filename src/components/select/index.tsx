3becf39379b279c84d038f84a2df1eb96db648c9
Augusto Ávila <augusto.avila@usemobile.xyz>
2021-09-05 17:47:38 -0300

    feat: add static select component

---
+++ src/components/select/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { makeStyles } from '@material-ui/core/styles';
+
+const select: React.FC = () => {
+
+  const UseStyles = makeStyles({
+    option: {
+      fontSize: 15,
+      '& > span': {
+        marginRight: 10,
+        fontSize: 18,
+      },
+    },
+  });
+
+  const classes = UseStyles();
+
+  return (
+    <Autocomplete 
+      options={top4Films}
+      classes={{
+        option: classes.option,
+      }}
+      getOptionLabel={(option) => option.title}
+      style={{ width: '25vw', margin: '0 auto' }}
+      renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
+    />
+  );
+}
+
+const top4Films = [
+  { title: 'The Shawshank Redemption', year: 1994 },
+  { title: 'The Godfather', year: 1972 },
+  { title: 'The Godfather: Part II', year: 1974 },
+  { title: 'The Dark Knight', year: 2008 }
+];
+
+export default select;
