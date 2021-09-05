14c4bf038a1f310c3b56de6b1d66949d358e5af9
Augusto Ávila <augusto.avila@usemobile.xyz>
2021-09-05 16:46:58 -0300

    feat: add index page to search a car

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
-
+import { Container, SubTitle, SearchDiv } from '../../styles/home';
+import Head from 'next/Head';
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <Container>
+      <Head>
+        <title>Consulta - Tabela Fipe</title>
+      </Head>
+      <h1>Tabela Fipe</h1>
+      <SubTitle>Consulte o valor de um veículo de forma gratuira</SubTitle>
+      <SearchDiv>
+
+      </SearchDiv>
+    </Container>
   )
 }
