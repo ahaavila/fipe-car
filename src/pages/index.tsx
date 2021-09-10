60cef6e54230bc1f33a4fb665cc9d4fa1c682b4b
Augusto Ávila <augusto.avila@usemobile.xyz>
2021-09-09 21:42:01 -0300

    feat: add redux on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,35 @@
+/* eslint-disable @next/next/link-passhref */
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Container, SubTitle, SearchDiv, LinkButton } from '../../styles/home';
 import Head from 'next/Head';
-import { connect, useDispatch, useSelector } from "react-redux";
-// import { getBranches } from '../states/state';
+import { useDispatch, useSelector } from "react-redux";
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
-import axios from 'axios';
-import { useFipe } from '../hooks/useFipe';
 import Select from '../components/select';
-import { fetchBranches, fetchModels, fetchYears } from '../store/actions/branchesAction';
+import { fetchBranches, fetchModels, fetchYears, setVehicle, fetchVehicle } from '../store/actions/branchesAction';
+import { vehicleProps, vehicleType } from '../utils/interface';
 
-// interface HomeStateProps {
-//   isGettingBranches: boolean,
-//   branchesList: Array<any>,
-// }
+interface stateProps {
+  vehicle: {
+    branches: vehicleType,
+    models: vehicleType,
+    years: vehicleType
+  }
+}
 
-// interface HomeDispatchProps {
-//   getBranches: typeof getBranches
-// }
-
-// interface HomeProps extends HomeDispatchProps { }
+interface vehicleStateProps {
+  vehicle: stateProps
+}
 
 export default function Home() {
-  const [newBranch, setNewBranch] = useState();
-  const [newModel, setNewModel] = useState();
-  const [newYear, setNewYear] = useState();
+  const [newBranch, setNewBranch] = useState<vehicleType>();
+  const [newModel, setNewModel] = useState<vehicleType>();
+  const [newYear, setNewYear] = useState<vehicleType>();
 
   const dispatch = useDispatch();
-  const { branches } = useSelector(state => state.vehicle);
-  const { models } = useSelector(state => state.vehicle);
-  const { years } = useSelector(state => state.vehicle);
+  const { branches }: stateProps = useSelector((state: vehicleStateProps) => state.vehicle);
+  const { models }: stateProps = useSelector((state: vehicleProps) => state.vehicle);
+  const { years }: stateProps = useSelector((state: vehicleProps) => state.vehicle);
   
   useEffect(() => {
     dispatch(fetchBranches());
@@ -36,15 +37,26 @@ export default function Home() {
 
   useEffect(() => {   
     if (newBranch !== undefined) {
-      dispatch(fetchModels(newBranch));
+      dispatch(fetchModels(newBranch.codigo));
     }
   }, [newBranch]);
 
   useEffect(() => {
     if (newModel !== undefined) {
-      dispatch(fetchYears(newBranch, newModel));
+      dispatch(fetchYears(newBranch.codigo, newModel.codigo));
     }
   }, [newModel]);
+
+  useEffect(() => {
+    if (newYear !== undefined) {
+      const data = {
+        brand: newBranch,
+        model: newModel,
+        year: newYear,
+      }
+      dispatch(setVehicle(data))
+    }
+  }, [newYear]);
   
   return (
     <Container>
@@ -54,9 +66,9 @@ export default function Home() {
       <h1>Tabela Fipe</h1>
       <SubTitle>Consulte o valor de um veículo de forma gratuita</SubTitle>
       <SearchDiv>
-        <Select selectLabel="Marca" selectOptions={branches ? branches : []} handleChange={(event: any, newValue)=> {setNewBranch(newValue.codigo)}}/>
-        <Select selectLabel="Modelo" selectOptions={models ? models : []} handleChange={(event: any, newValue) => {setNewModel(newValue.codigo)}}/>
-        <Select selectLabel="Ano" selectOptions={years ? years : []} handleChange={(event: any, newValue) => {setNewYear(newValue.codigo)}}/>
+        <Select selectLabel="Marca" selectOptions={branches ? branches : []} handleChange={(event: any, newValue)=> {setNewBranch(newValue)}}/>
+        <Select selectLabel="Modelo" selectOptions={models ? models : []} handleChange={(event: any, newValue) => {setNewModel(newValue)}}/>
+        <Select selectLabel="Ano" selectOptions={years ? years : []} handleChange={(event: any, newValue) => {setNewYear(newValue)}}/>
         <Link href="/result">
           <LinkButton>Consultar preço</LinkButton>
         </Link>
